2389c505a1b38928f1ed68209cdb9585184c3606
Adil Raees <adilraees911@gmail.com>
2025-10-30 05:02:34 +0500

    add images in insurance page

--- insurance-app-development-company-dubai.php
+++ insurance-app-development-company-dubai.php
@@ -195,7 +195,8 @@ include("includes/header.php");
                     </div>
                 </div>
                 <div class="col-lg-4">
-                    <img src="./assests/images/app-development-niche-design/insurance-hero.webp" alt="app-dev-insurance-hero-img">
+                    <img src="./assests/images/app-development-niche-design/insurance-hero.webp"
+                        alt="app-dev-insurance-hero-img">
                 </div>
             </div>
         </div>
@@ -385,29 +386,29 @@ include("includes/header.php");
         <div class="container" style="display: block; padding-top: 50px;">
             <div class="row">
                 <div class="col">
-                    <h2 class="text-white text-center same-all">Our <span>Financial App</span> Portfolio</h2>
+                    <h2 class="text-white text-center same-all">Our <span>Insurance App</span> Portfolio</h2>
                 </div>
             </div>
 
             <div class="row">
                 <div class="col-lg-9">
                     <div class="app-portfolio-img">
-                        <img src="assests/images/app-development/portfolio-01.webp" alt="portfolio-img-001">
-                        <img src="assests/images/app-development/portfolio-02.webp" alt="portfolio-img-002">
-                        <img src="assests/images/app-development/portfolio-03.webp" alt="portfolio-img-003">
-                        <img src="assests/images/app-development/portfolio-04.webp" alt="portfolio-img-004">
-                        <img src="assests/images/app-development/portfolio-05.webp" alt="portfolio-img-005">
-                        <img src="assests/images/app-development/portfolio-06.webp" alt="portfolio-img-006">
-                        <img src="assests/images/app-development/portfolio-07.webp" alt="portfolio-img-007">
-                        <img src="assests/images/app-development/portfolio-08.webp" alt="portfolio-img-006">
-                        <img src="assests/images/app-development/portfolio-09.webp" alt="portfolio-img-009">
-                        <img src="assests/images/app-development/portfolio-10.webp" alt="portfolio-img-0010">
-                        <img src="assests/images/app-development/portfolio-11.webp" alt="portfolio-img-0011">
-                        <img src="assests/images/app-development/portfolio-12.webp" alt="portfolio-img-0012">
+                        <img src="assests/images/insurance-app/portfolio-01.webp" alt="portfolio-insurance-img-001">
+                        <img src="assests/images/insurance-app/portfolio-02.webp" alt="portfolio-insurance-img-002">
+                        <img src="assests/images/insurance-app/portfolio-03.webp" alt="portfolio-insurance-img-003">
+                        <img src="assests/images/insurance-app/portfolio-04.webp" alt="portfolio-insurance-img-004">
+                        <img src="assests/images/insurance-app/portfolio-05.webp" alt="portfolio-insurance-img-005">
+                        <img src="assests/images/insurance-app/portfolio-06.webp" alt="portfolio-insurance-img-006">
+                        <img src="assests/images/insurance-app/portfolio-07.webp" alt="portfolio-insurance-img-007">
+                        <img src="assests/images/insurance-app/portfolio-08.webp" alt="portfolio-insurance-img-006">
+                        <img src="assests/images/insurance-app/portfolio-09.webp" alt="portfolio-insurance-img-009">
+                        <img src="assests/images/insurance-app/portfolio-10.webp" alt="portfolio-insurance-img-0010">
+                        <img src="assests/images/insurance-app/portfolio-11.webp" alt="portfolio-insurance-img-0011">
+                        <img src="assests/images/insurance-app/portfolio-12.webp" alt="portfolio-insurance-img-0012">
                     </div>
                 </div>
                 <div class="col-lg-3">
-                    <img src="assests/images/app-development/portfolio-13.webp" alt="portfolio-img-13">
+                    <img src="assests/images/insurance-app/portfolio-13.webp" alt="portfolio-insurance-img-13">
                 </div>
             </div>
         </div>
